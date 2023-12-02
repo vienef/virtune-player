5d9a77dbc08ca8a04bf3ebbfa93ad7578013c4c3
vienef <vionitanandaf@gmail.com>
2023-12-02 13:46:19 +0700

    Add tailwind css classes to the root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main>
+    <main className="p-6 lg:p-28 h-screen flex flex-col justify-center">
       <h1>Virtune</h1>
       <p>Tune in to Your Musical Virtues</p>
       <p>A prototype of an online music player, Virtune currently uses the NCS music library to create playlists.</p>
